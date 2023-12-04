8d4bc5e732773c732eb89783bdfc56619f883261
Agustín Montaña <81200417+Agustinm28@users.noreply.github.com>
2023-12-04 12:45:54 -0300

    unit_test

--- src/test/java/com/ar/edu/um/montana/cars/service/mapper/CarMapperTest.java
+++ src/test/java/com/ar/edu/um/montana/cars/service/mapper/CarMapperTest.java
@@ -1,7 +1,11 @@
 package com.ar.edu.um.montana.cars.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import com.ar.edu.um.montana.cars.domain.Car;
+import com.ar.edu.um.montana.cars.domain.Manufacturer;
+import com.ar.edu.um.montana.cars.service.dto.CarDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +16,61 @@ class CarMapperTest {
     @BeforeEach
     public void setUp() {
         carMapper = new CarMapperImpl();
+    }
+
+    @Test
+    public void testCarDtoToEntity() {
+        CarDTO carDTO = new CarDTO();
+        carDTO.setId(1L);
+        carDTO.setModel("Volkswagen Up");
+        carDTO.setYear("2018");
+        carDTO.setAvailable(3);
+
+        Car car = carMapper.toEntity(carDTO);
+
+        System.out.println(car);
+
+        assertEquals(car.getId(), carDTO.getId());
+        assertEquals(car.getModel(), carDTO.getModel());
+        assertEquals(car.getYear(), carDTO.getYear());
+        assertEquals(car.getAvailable(), carDTO.getAvailable());
+    }
+
+    @Test
+    public void testCarEntitytoDto() {
+        Car car = new Car();
+        car.setId(2L);
+        car.setModel("Tesla Model S");
+        car.setYear("2021");
+        car.setManufacturer(new Manufacturer().id(2L).name("Tesla"));
+        car.setAvailable(1);
+
+        CarDTO carDTO = carMapper.toDto(car);
+
+        System.out.println(carDTO);
+
+        assertEquals(car.getId(), carDTO.getId());
+        assertEquals(car.getModel(), carDTO.getModel());
+        assertEquals(car.getYear(), carDTO.getYear());
+        assertEquals(car.getAvailable(), carDTO.getAvailable());
+        assertEquals(car.getManufacturer().getId(), carDTO.getManufacturer().getId());
+    }
+
+    @Test
+    public void testNullCarDtoToEntity() {
+        CarDTO carDTO = new CarDTO();
+        carDTO.setId(null);
+        carDTO.setModel(null);
+        carDTO.setYear(null);
+        carDTO.setAvailable(null);
+
+        Car car = carMapper.toEntity(carDTO);
+
+        System.out.println(car);
+
+        assertNull(car.getId());
+        assertNull(car.getModel());
+        assertNull(car.getYear());
+        assertNull(car.getAvailable());
     }
 }
--- src/test/java/com/ar/edu/um/montana/cars/service/mapper/ManufacturerMapperTest.java
+++ src/test/java/com/ar/edu/um/montana/cars/service/mapper/ManufacturerMapperTest.java
@@ -1,7 +1,10 @@
 package com.ar.edu.um.montana.cars.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import com.ar.edu.um.montana.cars.domain.Manufacturer;
+import com.ar.edu.um.montana.cars.service.dto.ManufacturerDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +15,47 @@ class ManufacturerMapperTest {
     @BeforeEach
     public void setUp() {
         manufacturerMapper = new ManufacturerMapperImpl();
+    }
+
+    @Test
+    public void testManDtoToEntity() {
+        ManufacturerDTO manufacturerDTO = new ManufacturerDTO();
+        manufacturerDTO.setId(1L);
+        manufacturerDTO.setName("Volkswagen");
+
+        Manufacturer manufacturer = manufacturerMapper.toEntity(manufacturerDTO);
+
+        System.out.println(manufacturer);
+
+        assertEquals(manufacturer.getId(), manufacturerDTO.getId());
+        assertEquals(manufacturer.getName(), manufacturerDTO.getName());
+    }
+
+    @Test
+    public void testManEntityToDto() {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(2L);
+        manufacturer.setName("Tesla");
+
+        ManufacturerDTO manufacturerDTO = manufacturerMapper.toDto(manufacturer);
+
+        System.out.println(manufacturerDTO);
+
+        assertEquals(manufacturer.getId(), manufacturerDTO.getId());
+        assertEquals(manufacturer.getName(), manufacturerDTO.getName());
+    }
+
+    @Test
+    public void testNullManDtoToEntity() {
+        ManufacturerDTO manufacturerDTO = new ManufacturerDTO();
+        manufacturerDTO.setId(null);
+        manufacturerDTO.setName(null);
+
+        Manufacturer manufacturer = manufacturerMapper.toEntity(manufacturerDTO);
+
+        System.out.println(manufacturer);
+
+        assertNull(manufacturer.getId());
+        assertNull(manufacturer.getName());
     }
 }
